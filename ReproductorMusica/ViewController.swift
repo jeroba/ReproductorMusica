524a8a66719c296ad95c8d7595f68b8a84c7960b
Jesus Rodriguez Barrera <jeroba@gmail.com>
2016-03-20 21:08:03 -0600

    commit

--- ReproductorMusica/ViewController.swift
+++ ReproductorMusica/ViewController.swift
@@ -7,19 +7,127 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    private var reproductor: AVAudioPlayer!
+    var nombreCancion: String! = ""
+    var extensionCancion: String! = ""
+    
+    @IBOutlet weak var tituloCancion: UILabel!
+    @IBOutlet weak var portada: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tituloCancion.text = ""
+    }
+    
+    @IBAction func play() {
+        if !reproductor.playing{
+            reproductor.play()
+        }
+    }
+    
+    @IBAction func pausar() {
+        if reproductor.playing{
+            reproductor.pause()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func detener() {
+        if reproductor.playing{
+            reproductor.stop()
+            reproductor.currentTime = 0.0
+        }
     }
-
-
+    
+    @IBAction func volumen(sender: UISlider) {
+        reproductor.volume = sender.value
+    }
+    
+    @IBAction func aleatorio() {
+        var numeroCancion = Int(arc4random()) % 6
+        if numeroCancion == 0{
+            numeroCancion = 1
+        }
+        reproduceCancion(numeroCancion)
+    }
+    
+    @IBAction func cancionSeleccionada(sender: UIButton) {
+        
+        if sender.restorationIdentifier! == "Selena"{
+            reproduceCancion(1)
+        }else{
+            if sender.restorationIdentifier! == "Adele"{
+                reproduceCancion(2)
+            }else{
+                if sender.restorationIdentifier! == "Florida"{
+                    reproduceCancion(3)
+                }else{
+                    if sender.restorationIdentifier! == "Nicky"{
+                        reproduceCancion(4)
+                    }else{
+                        if sender.restorationIdentifier! == "Kelly"{
+                            reproduceCancion(5)
+                        }
+                    }
+                }
+            }
+        }
+        let cancionURL = NSBundle.mainBundle().URLForResource(nombreCancion, withExtension: extensionCancion)
+        
+        do {
+            try reproductor = AVAudioPlayer(contentsOfURL: cancionURL!)
+            reproductor.play()
+        }catch{
+            print("Error al cargar el archivo de la canción.")
+        }
+    }
+    
+    func reproduceCancion(numeroCancion: Int){
+        
+        switch numeroCancion{
+        case 1:
+            nombreCancion = "HandsToMySelf"
+            extensionCancion = "mp3"
+            self.tituloCancion.text = "Hands to myself - Selena Gomez"
+            self.portada.image = UIImage(named: "HandsToMySelf")
+        case 2:
+            nombreCancion = "Hello"
+            extensionCancion = "mp3"
+            self.tituloCancion.text = "Hello - Adele"
+            self.portada.image = UIImage(named: "Hello_cover")
+        case 3:
+            nombreCancion = "MyHouse"
+            extensionCancion = "mp3"
+            self.tituloCancion.text = "My house - Florida"
+            self.portada.image = UIImage(named: "MyHouse_cover")
+        case 4:
+            nombreCancion = "HastaElAmanecer"
+            extensionCancion = "mp3"
+            self.tituloCancion.text = "Hasta el amanecer - Nicky Jam"
+            self.portada.image = UIImage(named: "HastaElAmanecer_cover")
+        case 5:
+            nombreCancion = "PieceByPiece"
+            extensionCancion = "mp3"
+            self.tituloCancion.text = "Piece by piece - Kelly Clarkson"
+            self.portada.image = UIImage(named: "PieceByPiece_cover")
+        default:
+            print("default")
+        }
+        
+        let cancionURL = NSBundle.mainBundle().URLForResource(nombreCancion, withExtension: extensionCancion)
+        
+        do {
+            try reproductor = AVAudioPlayer(contentsOfURL: cancionURL!)
+            reproductor.play()
+        }catch{
+            print("Error al cargar el archivo de la canción.")
+        }
+    }
+    
+    
 }
 
